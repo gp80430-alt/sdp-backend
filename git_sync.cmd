ea2530d9e36f1abf5b2b513641c92edd7aea4e54
박영수 <gp80430@gmail.com>
2026-05-03 18:42:53 +0900

    Update: Synchronizing project changes

--- git_sync.cmd
+++ git_sync.cmd
@@ -1,14 +1,19 @@
 @echo off
+setlocal enabledelayedexpansion
+
 echo [1/4] Fixing Git ownership security...
+:: Add safe directory to avoid permission issues
 git config --global --add safe.directory "D:/박영수/1.프로그램/SDP"
 
 echo [2/4] Adding changes to Git...
 git add .
 
 echo [3/4] Committing changes...
-git commit -m "Update: Backend API URL to m09z version"
+:: Using a generic commit message, you can change this later
+git commit -m "Update: Synchronizing project changes"
 
 echo [4/4] Pushing to GitHub...
+:: This will prompt for login if the token was deleted
 git push
 
 echo.
